22208c5579cdb19a7e25aeaa097b64ea5a97e2c0
Asgar Ali <asgaralipq@gmail.com>
2020-08-08 15:36:44 +0530

    commit message

--- Dart/sample1.dart
+++ Dart/sample1.dart
@@ -1,1 +1,15 @@
-hello
+void main() { 
+    
+  var list = [1,2,3,6,7,2]; 
+    
+  var lar = list[0]; 
+  var sm = list[0]; 
+    
+  list.forEach((gfg) => { 
+        if (gfg > lar) {lar = gfg}, 
+        if (gfg < sm) {sm = gfg}, 
+      }); 
+  
+  print("Smallest value in the list : $sm"); 
+  print("Largest value in the list : $lar"); 
+}
